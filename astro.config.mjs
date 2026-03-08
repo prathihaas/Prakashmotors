0e99ef89e76802ce9d50aae18bfbf10dd8b984f2
prathihaas <prathihaasredd@gmail.com>
2026-03-08 14:51:49 +0530

    SEO: Add sitemap, robots.txt, SEO head component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+﻿import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://www.prakashmotors.in",
+  integrations: [sitemap()],
 });
